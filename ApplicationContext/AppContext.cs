90885b30de6c77b175762d2500dc213ae4e15b1c
erik <zu2017@yandex.ru>
2024-11-22 22:46:20 +0500

    ApplicationContext и строка подключения добавлены

---
+++ ApplicationContext/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hackaton.ApplicationContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using hackaton.ApplicationContext;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = "Data Source=PC\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
